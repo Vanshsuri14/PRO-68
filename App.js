71701559b3f057e5e689b61e05fe1db91d5b9d0e
Vanshsuri14 <64839142+Vanshsuri14@users.noreply.github.com>
2021-01-25 21:03:05 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigator';
+import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import fbScreen from './screens/fbScreen';
 import inScreen from './screens/inScreen';
